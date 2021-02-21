84d5033eaa06a6ef6431a4a22615e8d546dc6ea6
Nurkhat02 <78536893+Nurkhat02@users.noreply.github.com>
2021-02-21 15:29:08 +0600

    week 3
    week 3

---
+++ 21.py
@@ -0,0 +1,12 @@
+a = input().split()
+k = int(input())
+k = k%len(a)
+if k<0:
+    k = abs(k)
+    print(*a[k:],end =' ')
+    print(*a[0:k]) 
+elif k>=0:
+    k = abs(k)
+    print(*a[-k:],end =' ')
+    print(*a[0:-k])
+   
